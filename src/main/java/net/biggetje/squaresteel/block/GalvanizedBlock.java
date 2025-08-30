0d97301256af270cc2146ea9a73c23ecf3b6afb7
Biggetje <2011merijndebeer@gmail.com>
2025-08-30 16:24:35 +0200

    try 8, pls just work

--- src/main/java/net/biggetje/squaresteel/block/GalvanizedBlock.java
+++ src/main/java/net/biggetje/squaresteel/block/GalvanizedBlock.java
@@ -3,15 +3,16 @@ package net.biggetje.squaresteel.block;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 
-public class GalvanizedBlock extends HorizontalDirectionalBlock {
-    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
 
-    public GalvanizedBlock(Properties props) {
+public class GalvanizedBlock extends Block {
+    public static final DirectionProperty FACING = DirectionProperty.create("facing", Direction.Plane.HORIZONTAL);
+
+    public GalvanizedBlock(BlockBehaviour.Properties props) {
         super(props);
         this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
     }
@@ -23,7 +24,6 @@ public class GalvanizedBlock extends HorizontalDirectionalBlock {
 
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext context) {
-        // front (side2) moet altijd naar speler wijzen
         return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
     }
 }
