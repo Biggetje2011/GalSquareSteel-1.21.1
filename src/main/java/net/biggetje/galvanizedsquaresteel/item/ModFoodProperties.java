813616c00ceb1bdb96a1ede42043481d527efb74
merij <2011merijndebeer@gmail.com>
2025-12-29 09:01:19 +0100

    adding food

---
+++ src/main/java/net/biggetje/galvanizedsquaresteel/item/ModFoodProperties.java
@@ -0,0 +1,12 @@
+package net.biggetje.galvanizedsquaresteel.item;
+
+
+import net.minecraft.world.food.FoodProperties;
+
+public class ModFoodProperties {
+    public static final FoodProperties LUNCHBOX = new FoodProperties.Builder()
+            .nutrition(10)
+            .saturationModifier(1f)
+            .usingConvertsTo(ModItems.EMPTY_LUNCHBOX)
+            .build();
+}
--- src/main/java/net/biggetje/galvanizedsquaresteel/item/ModItems.java
+++ src/main/java/net/biggetje/galvanizedsquaresteel/item/ModItems.java
@@ -6,15 +6,26 @@ import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
+import net.biggetje.galvanizedsquaresteel.item.ModFoodProperties
+
+import javax.swing.*;
 
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(SquareSteel.MOD_ID);
 
     public static final DeferredItem<Item> STEEL_INGOT = ITEMS.register("steel_ingot",
-            () ->  new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties()));
 
     public static final DeferredItem<Item> CUTTER = ITEMS.register("cutter",
-            () ->  new CutterItem(new Item.Properties().durability(128)));
+            () -> new CutterItem(new Item.Properties()
+                    .durability(128)));
+
+    public static final DeferredItem<Item> LUNCHBOX = ITEMS.register("lunchbox",
+            () -> new Item(new Item.Properties()
+                    .food(ModFoodProperties.LUNCHBOX)));
+
+    public static final DeferredItem<Item> EMPTY_LUNCHBOX = ITEMS.register("empty_lunchbox",
+            () -> new Item(new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
